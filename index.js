c722ea261142f8b8aeec274ef8f047f4cd87e5aa
iqbalizz <iqbalizzudin13@gmail.com>
2023-09-08 23:08:28 +0700

    Update script, index.js fix

--- index.js
+++ index.js
@@ -17,26 +17,30 @@ function generateRandomPhoneNumber() {
     const randomPhoneNumber = `08${Math.floor(Math.random() * 1000000000).toString().padStart(9, '0')}`;
     return randomPhoneNumber;
 }
+//!FUNCTION DELAY
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 (async () => {
     //!Generate Random Phone
     const phoneNumber = generateRandomPhoneNumber();
-    const resultGetValue = await getValue();
-    const value = getBetween(resultGetValue, 'name="mobile_prefix" value="', '"');
     const inputJumlahRefferal = readlineSync.question(`[?] Masukkan Jumlah Refferal : `);
 
     for (let i = 0; i < inputJumlahRefferal; i++) {
-        for (let i = 0; i < inputJumlahRefferal; i++) {
-            const registerAcc = await getRegister(value, phoneNumber);
-            // Mengurai string JSON
-            const parsedResult = JSON.parse(registerAcc);
-            const getStatusRegister = getBetween(parsedResult, '{"msg":"', '"')
-            if (getStatusRegister === `login berhasil`) {
-                console.log(`[!] ${chalk.yellow(`Refferal ke-${i + 1}`)}`)
-                console.log(`[!] ${chalk.green(`Succes Registrasi`)}`)
-            } else {
-                console.log(`[!] ${chalk.red(`Gagal Registrasi`)}`)
-            }
+        const resultGetValue = await getValue();
+        const value = getBetween(resultGetValue, 'name="mobile_prefix" value="', '"');
+        const registerAcc = await getRegister(value, phoneNumber);
+        // Mengurai string JSON
+        const parsedResult = JSON.parse(registerAcc);
+        const getStatusRegister = getBetween(parsedResult, '{"msg":"', '"')
+        if (getStatusRegister === `login berhasil`) {
+            console.log(`[!] ${chalk.yellow(`Refferal ke-${i + 1}`)}`)
+            console.log(`[!] ${chalk.green(`Succes Registrasi`)}`)
+        } else {
+            console.log(`[!] ${chalk.red(`Gagal Registrasi`)}`)
         }
+        await delay(2000)
     }
+
 })();
